1c5caeb038a6247dbf84ff254811ceee29a5ae22
Hamdi_Dev <128970559+Hamdiaja@users.noreply.github.com>
2024-05-17 15:56:21 +0700

    error style

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,10 +2,10 @@ import {useRouterError} from "react-router-dom";
 const ErrorPage = () => {
  const error = useRouterError();
  return ( 
-    <div>
-  <h1>Oops!</h1>
-  <p>Sorry, an unexpected error has occurred.</p>
-  <p>{error.statusText || error.message}
+    <div className="flex justify-center min-h-screen items-center">
+  <h1 className="font-bold text-8xl">Oops!</h1>
+  <p className="mb-2 my-2 text-2xl font-bold">Sorry, an unexpected error has occurred.</p>
+  <p className="mb-2">{error.statusText || error.message}
   </p>
   </div>
   )
